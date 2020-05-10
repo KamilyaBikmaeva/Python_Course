8a76cfd0f5d5947b541cb8e21e4f1069b7db3511
KamilyaBikmaeva <slbk.bbb@gmail.com>
2020-05-11 00:54:05 +0300

    Add BinaryTree

---
+++ Binary/BinaryTree.py
@@ -0,0 +1,39 @@
+class TreeNode:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = TreeNode(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = TreeNode(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data),
+        if self.right:
+            self.right.print_tree()
+
+
+root = TreeNode(15)
+root.insert(5)
+root.insert(17)
+root.insert(18)
+root.insert(3)
+root.insert(8)
+root.insert(28)
+
+root.print_tree()
